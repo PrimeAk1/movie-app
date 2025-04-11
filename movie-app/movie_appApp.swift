6db61fec2ea8ba706cdb83c6308dc8aaaafb50e4
PrimeAk1 <137424375+PrimeAk1@users.noreply.github.com>
2025-04-11 15:58:37 +0200

    Initial commit

---
+++ movie-app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie-app
+//
+//  Created by Akos  Gegeny   on 2025. 04. 11..
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
