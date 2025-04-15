860de9972d63885d21e1ee5dd4fb36d735dbdd1f
PrimeAk1 <137424375+PrimeAk1@users.noreply.github.com>
2025-04-15 20:04:34 +0200

    added FetchMovieRequest and MoviePageResponse

---
+++ movie-app/dtos/requests/FetchMovieRequest.swift
@@ -0,0 +1,16 @@
+//
+//  FetchMovieRequest.swift
+//  movie-app
+//
+//  Created by Akos  Gegeny   on 2025. 04. 15..
+//
+
+struct FetchMoviesRequest {
+    let accessToken: String = Config.bearerToken
+    let genreId: Int
+    
+    func asRequestParams() -> [String: Any] {
+        return ["with_genres": genreId]
+    }
+}
+
